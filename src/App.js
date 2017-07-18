b1f4041ff084750879f4ecdd5959f32b39c2091c
Tom Martensen <mail@tommartensen.de>
2017-07-18 14:25:47 +0200

    bootstrapped hello world

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+global.jQuery = require('jquery');
+global.Tether = require('tether');
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          Hello World!
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container-fluid">
+				<h1>Welcome</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
