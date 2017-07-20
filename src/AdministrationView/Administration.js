e2cbb892e1f9ff4fa907b8d5e9ebf5e375a17e63
Tom Martensen <mail@tommartensen.de>
2017-07-20 09:57:30 +0200

    created basic views for dashboard and administration

---
+++ src/AdministrationView/Administration.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Administration extends Component {
+	render() {
+		return (
+			<div>
+				Administration
+			</div>
+		);
+	}
+}
+
+export default Administration;
