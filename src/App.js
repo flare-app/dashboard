90817459d0b2dd963195c6a0d6a4dd95f62e9cc1
Frederike Ramin <frederike.ramin@student.hpi.de>
2017-07-20 11:10:58 +0200

    Include Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import './App.css';
 import LoginUtils from "./LoginView/LoginUtils";
 import LoginForm from "./LoginView/LoginForm";
 import Dashboard from "./DashboardView/Dashboard";
+import Administration from "./AdministrationView/Administration";
 global.jQuery = require('jquery');
 global.Tether = require('tether');
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.min.css');;
 
 class App extends Component {
+	constructor() {
+		super();
+		this.changeView = this.changeView.bind(this);
+		this.state = {
+			currentView: <Dashboard changeView={this.changeView}/>
+		};
+	}
+
+	changeView(newView) {
+		switch(newView) {
+			case "Admin":
+				this.setState({
+					currentView: <Administration changeView={this.changeView}/>
+				});
+				break;
+			default:
+				this.setState({
+					currentView: <Dashboard changeView={this.changeView}/>
+				});
+				break;
+		}
+
+	}
+
 	render() {
 		if (!LoginUtils.isLoggedIn()) {
 			return <LoginForm/>
 		}
-		return (
-			<Dashboard/>
-		);
+		return this.state.currentView;
 	}
 }
 
--- src/DashboardView/Dashboard.js
+++ src/DashboardView/Dashboard.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Header from "../Header";
 
 class Dashboard extends Component {
+
 	render() {
 		return (
 			<div>
+				<Header changeView={this.props.changeView}/>
 				Dashboard
 			</div>
 		);
